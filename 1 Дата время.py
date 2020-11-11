54aeea0ec7d8a7fe6639ffb3799b853f43818a8e
iForme174 <72073044+iForme174@users.noreply.github.com>
2020-11-11 09:10:35 +0500

    Add files via upload

---
+++ 1 Дата время.py
@@ -0,0 +1,101 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> help
+Type help() for interactive help, or help(object) for help about object.
+>>> help("keywords")
+
+Here is a list of the Python keywords.  Enter any keyword to get more help.
+
+False               break               for                 not
+None                class               from                or
+True                continue            global              pass
+__peg_parser__      def                 if                  raise
+and                 del                 import              return
+as                  elif                in                  try
+assert              else                is                  while
+async               except              lambda              with
+await               finally             nonlocal            yield
+
+>>> help("list")
+
+>>> roup
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    roup
+NameError: name 'roup' is not defined
+>>> group
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    group
+NameError: name 'group' is not defined
+>>> \\IS-DC
+SyntaxError: unexpected character after line continuation character
+>>> dict
+<class 'dict'>
+>>> end
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    end
+NameError: name 'end' is not defined
+>>> print
+<built-in function print>
+>>> print("даров")
+даров
+>>> print("Здарова")
+Здарова
+>>> int a=1
+SyntaxError: invalid syntax
+>>> int
+<class 'int'>
+>>> a=4
+>>> b=2
+>>> a+b
+6
+>>> float
+<class 'float'>
+>>> c=6.9
+>>> complex
+<class 'complex'>
+>>> 2a
+SyntaxError: invalid syntax
+>>> int
+<class 'int'>
+>>> 2a
+SyntaxError: invalid syntax
+>>> float
+<class 'float'>
+>>> 2a
+SyntaxError: invalid syntax
+>>> print("a+b-c")
+a+b-c
+>>> print(a+b-c)
+-0.9000000000000004
+>>> print(a+b)
+6
+>>> a+b
+6
+>>> a=9
+>>> a+b
+11
+>>> a-a(b)
+Traceback (most recent call last):
+  File "<pyshell#30>", line 1, in <module>
+    a-a(b)
+TypeError: 'int' object is not callable
+>>> a-a^2
+2
+>>> str = "28/10/2020"
+>>> a = str.split("/")
+>>> a
+['28', '10', '2020']
+>>> en = a[2] + a[1] + a[0]
+>>> en
+'20201028'
+>>> en = a[2] + '-' + a[1] + '-' a[0]
+SyntaxError: invalid syntax
+>>> en = a[2] + '-' + a[1] + '-' + a[0]
+>>> en
+'2020-10-28'
+>>> print ('дата', en)
+дата 2020-10-28
+>>> 
